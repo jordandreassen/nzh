027c001744a0a7fb0b8a31ac54d1ab419d617237
Jordan Andreassen <jordan@flatradar.co.nz>
2020-05-19 15:55:15 +1200

    v0.0.5 - Should block all '/offers' scripts

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) { 
         return {cancel: true}; },
     {urls: [
-        "https://www.nzherald.co.nz/pb/gr/c/default/r0nARN1TvOjoZr/headjs/75b5749904.js?v=169"
+        "https://www.nzherald.co.nz/*75b5749904.js?v=169"
     ]},
     ["blocking"]);
